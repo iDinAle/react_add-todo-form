c7bafe2a574efc56cf4402bcbb153cbd92f879cb
Alexii Aidinov <alexii.aidinov@gmail.com>
2019-12-11 00:12:00 +0200

    Updates

---
+++ precache-manifest.f42ad581950f5a504ea240fdaaa6dfcd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17e1de5113d406daa0471a35e36ef8b",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "d3ce9b1aa06191eb95f4",
+    "url": "/react_add-todo-form/static/css/main.56aa641f.chunk.css"
+  },
+  {
+    "revision": "3b9860065277c4b3e3a8",
+    "url": "/react_add-todo-form/static/js/2.692255d0.chunk.js"
+  },
+  {
+    "revision": "d3ce9b1aa06191eb95f4",
+    "url": "/react_add-todo-form/static/js/main.40177df5.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
